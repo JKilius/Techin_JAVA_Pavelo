4e6d4182334f72ad82c34621b73e4814fadb8ac0
Pavelslc <salc.pavel1@gmail.com>
2024-02-01 09:14:57 +0200

    Started FilteringAndSortingTest

---
+++ FilteringAndSortingTest/src/main/java/lt/techin/pavels/filteringandsorting/page/BasePage.java
@@ -0,0 +1,14 @@
+package lt.techin.pavels.filteringandsorting.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
