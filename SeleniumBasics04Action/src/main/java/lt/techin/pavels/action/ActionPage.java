99282f118b74282e7983a3e4aa31527cdc714483
Pavelslc <salc.pavel1@gmail.com>
2024-02-09 19:31:03 +0200

    finished implementing all actions methods in Page and Test classes

--- SeleniumBasics04Action/src/main/java/lt/techin/pavels/action/ActionPage.java
+++ SeleniumBasics04Action/src/main/java/lt/techin/pavels/action/ActionPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+import java.util.List;
 
 public class ActionPage {
     private WebDriver driver;
@@ -18,6 +18,21 @@ public class ActionPage {
     WebElement boxDraggable;
     @FindBy(css = "#droppable")
     WebElement boxDroppable;
+    @FindBy(css = "#droppable p")
+    WebElement boxDroppableBackgroundColorHolder;
+    @FindBy(css = "#double-click")
+    WebElement boxDoubleClick;
+    @FindBy(xpath = "//button[@class=\"dropbtn\"][text()='Hover Over Me First!']")
+    WebElement buttonHoverOverFirst;
+    @FindBy(xpath = "//button[@class=\"dropbtn\"][text()='Hover Over Me Second!']")
+    WebElement buttonHoverOverSecond;
+    @FindBy(xpath = "//button[@class=\"dropbtn\"][text()='Hover Over Me Third!']")
+    WebElement buttonHoverOverThird;
+    @FindBy(css = ".dropdown-content a")
+    List<WebElement> dropDownElements;
+    @FindBy (css = "#click-box")
+    WebElement clickBox;
+
 
     public ActionPage(WebDriver driver) {
         this.driver = driver;
@@ -29,4 +44,45 @@ public class ActionPage {
     public void dragBoxDraggable() {
         actions.dragAndDrop(boxDraggable, boxDroppable).perform();
     }
+
+    public String getBoxDroppableBackgroundColor() {
+        return boxDroppableBackgroundColorHolder.getCssValue("background-color");
+    }
+
+    public String getBoxDroppableText() {
+        return boxDroppable.getText();
+    }
+    public void doubleClickBoxDoubleClick(){
+        actions.doubleClick(boxDoubleClick).perform();
+    }
+
+    public String getBoxDoubleClickBackgroundColor(){
+        return boxDoubleClick.getCssValue("background-color");
+    }
+    public void mouseMoveOverButtonFirst(){
+        actions.moveToElement(buttonHoverOverFirst).perform();
+    }
+    public void mouseMoveOverButtonSecond(){
+        actions.moveToElement(buttonHoverOverSecond).perform();
+    }
+    public void mouseMoveOverButtonThird(){
+        actions.moveToElement(buttonHoverOverThird).perform();
+    }
+
+    public int getCountOfVisibleSubMenuElements() {
+       return (int) dropDownElements.stream().filter(WebElement::isDisplayed).count();
+    }
+
+    public void clickDownClickBox() {
+        actions.clickAndHold(clickBox).perform();
+    }
+
+    public String getClickBoxBackgroundColor() {
+       return clickBox.getCssValue("background-color");
+    }
+
+    public String getClickBoxText() {
+        return clickBox.getText();
+    }
+
 }
--- SeleniumBasics04Action/src/test/java/lt/techin/pavels/action/test/ActionTest.java
+++ SeleniumBasics04Action/src/test/java/lt/techin/pavels/action/test/ActionTest.java
@@ -1,27 +1,94 @@
 package lt.techin.pavels.action.test;
 
 import lt.techin.pavels.action.ActionPage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ActionTest {
     private WebDriver driver;
-    private Actions actions;
     private ActionPage actionPage;
 
     @BeforeEach
-    void setup(){
-    driver = new ChromeDriver();
-    driver.get("https://webdriveruniversity.com/Actions/index.html");
-    actionPage = new ActionPage(driver);
-
-    driver.manage().window().maximize();
+    void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://webdriveruniversity.com/Actions/index.html");
+        actionPage = new ActionPage(driver);
+        driver.manage().window().maximize();
     }
+
     @Test
-    void TempTest(){
+    void clickAndHoldClickBoxTest() {
+        // Click and hold clickBox
+        actionPage.clickDownClickBox();
+
+        // Set and get expected and actual values of clickBox background color
+        String expectedClickBoxBackgroundColor = "rgba(0, 255, 0, 1)";
+        String actualClickBoxBackgroundColor = actionPage.getClickBoxBackgroundColor();
+        String messageOnColorAssertFail = "Asserting visible background color:";
+
+        // Set and get expected and actual values of clickBox text
+        String expectedClickBoxText = "Well done! keep holding that click now.....";
+        String actualClickBoxText = actionPage.getClickBoxText();
+        String messageOnTextAssertFail = "Expected visible text: Well done! keep holding that click now.....";
+
+        assertEquals(expectedClickBoxBackgroundColor, actualClickBoxBackgroundColor, messageOnColorAssertFail);
+        assertEquals(expectedClickBoxText, actualClickBoxText, messageOnTextAssertFail);
+    }
+
+    @Test
+    void hoverOverButtonsTest() {
+        actionPage.mouseMoveOverButtonFirst();
+        assertEquals(1, actionPage.getCountOfVisibleSubMenuElements(), "Expected number of visible dropdown elements under first button = 1");
+
+        actionPage.mouseMoveOverButtonSecond();
+        assertEquals(1, actionPage.getCountOfVisibleSubMenuElements(), "Expected number of visible dropdown elements under second button = 1");
+
+        actionPage.mouseMoveOverButtonThird();
+        assertEquals(2, actionPage.getCountOfVisibleSubMenuElements(), "Expected number of visible dropdown elements under third button = 2");
+    }
+
+
+    @Test
+    void doubleClickingBoxTest() {
+        // double clicking doubleClick box
+        actionPage.doubleClickBoxDoubleClick();
+
+        // set and get expected and actual doubleClickBox background color
+        String expectedClickBoxBackgroundColor = "rgba(147, 203, 90, 1)";
+        String actualClickBoxBackgroundColor = actionPage.getBoxDoubleClickBackgroundColor();
+
+        // asserting doubleClickBox background color
+        assertEquals(expectedClickBoxBackgroundColor, actualClickBoxBackgroundColor, "Asserting doubleClickBox background color:");
+    }
+
+    @Test
+    void draggingBoxDroppableTest() {
+        // drag draggable box on droppable box
         actionPage.dragBoxDraggable();
+
+        // set and get expected and actual values of droppable box background color
+        String expectedBoxDroppableBackgroundColor = "rgba(244, 89, 80, 1)";
+        String actualBoxDroppableBackgroundColor = actionPage.getBoxDroppableBackgroundColor();
+
+
+        // expected and actual values of droppable box text
+        String expectedBoxDroppableText = "Dropped!";
+        String actualBoxDroppableText = actionPage.getBoxDroppableText();
+
+        // asserting droppable box background color and text
+        assertEquals(expectedBoxDroppableBackgroundColor, actualBoxDroppableBackgroundColor, "Asserting droppable box background color:");
+        assertEquals(expectedBoxDroppableText,actualBoxDroppableText,"Asserting droppable box text:");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
     }
 }
